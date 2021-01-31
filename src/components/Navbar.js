7d33e88004014ad254496acf2abb5ba973b43683
Evan Kim <evankim20@gmail.com>
2021-01-31 09:23:34 -0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul id="nav">
+                <li>evan kim</li>
+                <li>about</li>
+                <li>experience</li>
+                <li>education</li>
+                <li>skills</li>
+                <li>projects</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
+
